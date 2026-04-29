184b05f4762383b5ba7c6ebdd331148664031068
Bibash Shrestha <bibashshrestha@gmail.com>
2026-04-29 19:55:55 +0545

    project-setup

---
+++ lib/minimax_tictactoe.dart
@@ -0,0 +1,10 @@
+/// minimax_tictactoe, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate minimax_tictactoe
+/// dart pub global activate minimax_tictactoe
+///
+/// # see usage
+/// minimax_tictactoe --help
+/// ```
+library;
